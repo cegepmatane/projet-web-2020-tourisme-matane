243aa0157fff044b2a0a19392358c2deff028241
Florian Landry <florian.landry71@gmail.com>
2020-01-29 10:57:12 -0500

    infos_achat_offre.html changé en .php plus ajout de code dedans

---
+++ Templates/infos_achat_offre.php
@@ -0,0 +1,78 @@
+<?php
+    include ('../Scripts/get_all_destinations.php');
+    $res = get_all_destination();
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Tourisme Matane</title>
+    <link rel="stylesheet" href="../CSS/index.css">
+    <link rel="stylesheet" type="text/css" href="../CSS/infos_achat_offre.css">
+	<base target="_parent">
+</head>
+<body>
+	<header>
+
+    	<iframe src="header.html" align="top" frameborder="0" marginheight="0" marginwidth="0" style="margin: -0.5vw -0vw -0.5vw -0.5vw;width: 100vw;height: 8vw;"></iframe>
+
+  </header>
+<hr/> 
+<div class="row">
+  <div class="side">
+    <?php
+        echo "<img src=\"".$res[2]["url_image"]."\" class=\"img-destination\" alt=\"Image représentant la destination\"/>";
+    ?>
+  </div>
+
+  <div class="main">
+    <h1 id="nom_offre">Visite de la ville de Matane</h1>
+    <?php
+        echo "<p class=\"text-destination\">".$res[2]["description"]."</p>";
+    ?>
+  </div>
+
+  <div class="main">
+    <p>
+      Du <input type="date" name="debut"> au <input type="date" name="fin"> (jours selectionnés inclus)
+    </p>
+  </div>
+  <div class="main">
+    <h2>Nombre de personnes</h2>
+    <ul class="personnes">
+      <li><input type="number" name="adultes"> adultes(+18ans)</li>
+      <li><input type="number" name="enfants"> enfants(-18ans)</li>
+      <li><input type="number" name="animaux"> animaux</li>
+    </ul>
+  </div>
+
+  <div class="main">
+    <ul class="liste">
+      Lieux inclus :
+      <li>Une superbe école</li>
+      <li>Un splendide quartier</li>
+      <li>Un magnifique parc</li>
+    </ul>
+    <ul class="liste">
+      Options incluses :
+      <li>Un Wi-Fi surpuissant</li>
+      <li>Un trajet rapide en bus</li>
+      <li>L'entrée dans les lieux</li>
+    </ul>
+    <div id="achat">
+      <p>
+          <?php
+              echo $res[2]["prix"]." €";
+          ?>
+      </p>
+      <button>Acheter</button>
+    </div>
+  </div>
+
+</div>
+<hr/>
+	<footer>
+		<iframe src="footer.html" align="bottom" frameborder="0" marginheight="0" marginwidth="0" style="margin: -0.5vw -0vw -0.5vw -0.5vw;width: 100vw;height: 8vw;"></iframe>
+	</footer>
+</body>
+</html>
