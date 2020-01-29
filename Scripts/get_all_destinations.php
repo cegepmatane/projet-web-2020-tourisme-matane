947015946b07ef645a2fa357abad8706ab694736
Loris <loris.ponroy@lilo.org>
2020-01-29 09:58:24 -0500

    Script pour #12, récupérer toutes les offres

--- Scripts/get_all_destinations.php
+++ Scripts/get_all_destinations.php
@@ -1,8 +1,20 @@
 <?php
 
+    include('database_data.php');
 
+    $db = new PDO("$server:host=$host;dbname=$base",$user, $pass);
 
-    return null;
-
-
-
+    $sql_command = "SELECT * from OFFRE WHERE sur_accueil=true";
+    $req = $this->db->query($sql_command);
+    $i = 0;
+    $answer = array();
+    foreach ($db->query($sql_command) as $tab) {
+        $answer[$i] = array(
+            'prix' => $tab['prix'],
+            'description' => $tab['description'],
+            'location' => $tab['location'],
+            'ville' => $tab['ville']
+        );
+        $i++;
+    }
+    return($answer);
