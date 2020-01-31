ca005a21cedca46828ac9dc7bc524e31aa862884
Quentin Meignan <2001068@cgmatane.qc.ca>
2020-01-31 09:58:17 -0500

    #14 connexion à la bd via connexion.php

--- Scripts/add_destination.php
+++ Scripts/add_destination.php
@@ -1,13 +1,7 @@
 <?php
 function add_destination($debut, $duree, $prix, $img, $description, $ville)
 {
-    $server = "mysql";
-    $host = "localhost";
-    $base = "bddtourisme";
-    $user = "root";
-    $pass = "";
-
-    $db = new PDO("$server:host=$host;dbname=$base", $user, $pass);
+    include("connexion.php");
 
     $sql_command = "INSERT INTO OFFRE (debut, duree, prix, url_image, description, ville)
                                 VALUES ('$debut', '$duree', '$prix', '$img', '$description', '$ville')";
