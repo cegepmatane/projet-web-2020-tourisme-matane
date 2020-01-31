fa53a0cd4c9f962a469aa3667eefd62946d396ab
Loris <loris.ponroy@lilo.org>
2020-01-31 10:53:03 -0500

    Création du panneau d'administration + affichage des offres

--- Scripts/get_all_destinations.php
+++ Scripts/get_all_destinations.php
@@ -1,13 +1,7 @@
 <?php
     function get_all_destination()
     {
-        $server = "mysql";
-        $host = "localhost";
-        $base = "bddtourisme";
-        $user = "root";
-        $pass = "";
-
-        $db = new PDO("$server:host=$host;dbname=$base", $user, $pass);
+        include ('connexion.php');
 
         $sql_command = "SELECT * from OFFRE";
         $i = 0;
