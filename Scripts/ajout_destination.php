4c9faf2370776f17ece8e22e9f9def26a1786445
Quentin Meignan <mr.sloriss@gmail.com>
2020-02-03 17:11:26 -0500

    #21 création de la vue d'ajout d'une offre et liaison avec le script

---
+++ Scripts/ajout_destination.php
@@ -0,0 +1,19 @@
+<?php
+    include("connexion.php");
+    global $db;
+   
+    if (isset($_POST['submit']))
+    {
+        $ville = addslashes(trim($_POST['ville']));
+        $description = addslashes(trim($_POST['description']));
+        $image = addslashes(trim($_POST['image']));
+        $debut = addslashes(trim($_POST['debut']));
+        $duree = addslashes(trim($_POST['duree']));
+        $prix = addslashes(trim($_POST['prix']));
+    
+        $sql_command = "INSERT INTO OFFRE (debut, duree, prix, url_image, description, ville)
+                                    VALUES ('$debut', '$duree', '$prix', '$image', '$description', '$ville')";
+        echo $sql_command;
+        $db->query($sql_command);
+    }
+?>
