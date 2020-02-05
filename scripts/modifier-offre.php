3f9995be51a1b6260e774ea29b3b7aa38746764f
Alexandre Roussel <alexandreroussel.fr@gmail.com>
2020-02-05 11:19:49 -0500

    #26 correction pour modifier-offre

--- scripts/modifier-offre.php
+++ scripts/modifier-offre.php
@@ -3,13 +3,14 @@
 
     $filtresOffre = array(
         'id' => FILTER_VALIDATE_INT,
+        'debut' => FILTER_SANITIZE_STRING,
         'duree' => FILTER_VALIDATE_INT,
-        'prix' => FILTER_VALIDATE_INT,
-        'image' => FILTER_VALIDATE_INT,
-        'description' => FILTER_VALIDATE_INT,
-        'ville' => FILTER_VALIDATE_INT
+        'prix' => FILTER_VALIDATE_FLOAT,
+        'image' => FILTER_SANITIZE_URL,
+        'description' => FILTER_SANITIZE_STRING,
+        'ville' => FILTER_SANITIZE_STRING
     );
-    $
+    $offre = filter_input_array(INPUT_POST, $filtresOffre);
 
     $SQL_MODIFIER_OFFRE = "UPDATE offre SET debut = :debut,
         duree = :duree,
@@ -20,12 +21,13 @@
         WHERE id_offre = :id";
 
     $modificationOffre = $db->prepare($SQL_MODIFIER_OFFRE);
-    $modificationOffre->bindParam(':id', $filtresOffre['id'], PDO::PARAM_INT);
-    $modificationOffre->bindParam(':duree', $filtresOffre['duree'], PDO::PARAM_INT);
-    $modificationOffre->bindParam(':prix', $filtresOffre['prix'], PDO::PARAM_INT);
-    $modificationOffre->bindParam(':image', $filtresOffre['image'], PDO::PARAM_STRING);
-    $modificationOffre->bindParam(':desciption', $filtresOffre['description'], PDO::PARAM_STRING);
-    $modificationOffre->bindParam(':ville', $filtresOffre['ville'], PDO::PARAM_STRING);
+    $modificationOffre->bindParam(':id', $offre['id'], PDO::PARAM_INT);
+    $modificationOffre->bindParam(':debut', $offre['id'], PDO::PARAM_STR);
+    $modificationOffre->bindParam(':duree', $offre['duree'], PDO::PARAM_FLOAT);
+    $modificationOffre->bindParam(':prix', $offre['prix'], PDO::PARAM_INT);
+    $modificationOffre->bindParam(':image', $offre['image'], PDO::PARAM_STR);
+    $modificationOffre->bindParam(':desciption', $offre['description'], PDO::PARAM_STR);
+    $modificationOffre->bindParam(':ville', $offre['ville'], PDO::PARAM_STR);
     $modificationOffre->execute();
     
     header("Location: ../pages/panneau-admin.php");
