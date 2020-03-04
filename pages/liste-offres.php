d01bb8df2980ad25d9552d31e0cdf7b42a5adeee
Alexandre Roussel <alexandreroussel.fr@gmail.com>
2020-03-04 11:39:45 -0500

    #41 Projet ajax fonctionnel sans mise en forme

--- pages/liste-offres.php
+++ pages/liste-offres.php
@@ -1,6 +1,3 @@
-<?php
-$id = 1;
-?>
 <!DOCTYPE html>
 <html lang="fr">
     <head>
@@ -10,25 +7,44 @@ $id = 1;
         <link rel="stylesheet" href="../css/theme.css">
         <link rel="stylesheet" href="../css/liste-offres.css">
         <base target="_parent">
+
+        <script type="text/javascript" src="../librairies/Ajax.js"></script>
+        <script type="text/javascript">
+            function recevoirOffres(ajax)
+            {
+                console.log("recevoirOffres()");
+                console.log("reponse="+ajax.responseText);
+                document.getElementById('liste-offres').innerHTML = ajax.responseText;
+            }
+
+            function actualiserOffres()
+            {
+                console.log('actualiserOffres()');
+                ajax = new Ajax();
+			    console.log(ajax);
+			    ajax.executer("GET", "http://localhost/projet-web/projet-web-2020-tourisme-matane/pages/tri-offres.php",
+                    "ville="+document.getElementById('champ-recherche').value, recevoirOffres);
+            }
+        </script>
     </head>
     <body>
         <?php include("header.php"); ?>
         <hr/>
         <!-- Filtre et recherche -->
-        <!-- <div> 
-            <form action="GET" class="formulaire">
-                <input type="text" value="Destination"/>
-                <select name="filtre">
+        <div> 
+            <form action="#" id="recherche">
+                <input id="champ-recherche" type="search" onkeyup="actualiserOffres()" class="barre-recherche" placeholder="Ville">
+                <!-- <select name="filtre">
                     <option value="nouveaute">Nouveauté</option>
                     <option value="prix_moins_to_plus">Du - cher au + cher</option>
                     <option value="prix_plus_to_moins">Du + cher au - cher</option>
                     <option value="genre">Genre</option>
                     <option value="alphabétique">Ordre alphabétique</option>
                     <option value="alphabétique_inverse">Ordre alphabétique inverse</option>
-                </select>
+                </select> -->
             </form>
         </div>
-        <div>
+        <!-- <div>
             <form action="">
                 <input type="date">
                 Prix : <input class="prix" type="range" name="points" min="0" max="10">
@@ -43,30 +59,25 @@ $id = 1;
         </div> -->
         
         <!-- listage des offres -->
-        <div id="lig">
-            <input type="hidden" name="id" value="">
-            <!-- Boucle pour chaque activité -->
-            <?php
-            include('../Scripts/recuperer-destinations.php');
-            foreach (recupererDestinations() as $tab){
+        <div id="liste-offres">
+            <?php include('../Scripts/recuperer-destinations.php');
+            foreach (recupererDestinations() as $offres){
                 $html = "<div class=\"offre\">";
-                $html .= "<img src=\"".$tab["url_image"]."\" class=\"img-destination\" alt=\"Image représentant la destination\"/>";
-                $html .= "<h1>Ville : ".$tab["ville"]."</h1>";
-                $html .= "<p> Départ : ".$tab["debut"]."</p>";
-                $html .= "<p> Durée en jours : ".$tab["duree"]."</p>";
-                $html .= "<p> Prix initial : ".$tab["prix"]." €</p>";
+                $html .= "<img src=\"".$offres['url_image']."\" class=\"img-destination\" alt=\"Image représentant la destination\"/>";
+                $html .= "<h1>Ville : ".$offres['ville']."</h1>";
+                $html .= "<p> Départ : ".$offres['debut']."</p>";
+                $html .= "<p> Durée en jours : ".$offres['duree']."</p>";
+                $html .= "<p> Prix initial : ".$offres['prix']." €</p>";
                 $html .= "<form action=\"./infos-achat-offre.php\" method=\"get\">";
-                $html .= "<input type=\"hidden\" name=\"id\" value=\"".$id."\"> ";
+                $html .= "<input type=\"hidden\" name=\"id\" value=\"".$offres['id_offre']."\"> ";
                 $html .= "<input id=\"button\" type=\"submit\" value=\">> Plus d'info\">";
                 $html .= "</form> ";
                 $html .= "</div>";
                 echo $html;
-                $id++;
-            }
-            ?>
-
+            } ?>
         </div>
         <hr/>
+
         <?php include("footer.html"); ?>
     </body>
 </html>
