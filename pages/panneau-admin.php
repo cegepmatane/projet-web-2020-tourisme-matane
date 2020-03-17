1d0d7f6525a5a164e149be5bc27d10a6c4ffafe8
Loris <loris.ponroy@lilo.org>
2020-03-17 16:26:18 -0400

    Ajax pour modifier offre dans panneau admin

--- pages/panneau-admin.php
+++ pages/panneau-admin.php
@@ -9,21 +9,43 @@
         <title>Tourisme-Matane</title>
         <script type="text/javascript" src="../librairies/Ajax.js"></script>
         <script>
+            let pDescription;
             let inputDescription;
+            let idOffre;
+
+            let divOffre;
         </script>
         <script>
             function recevoirMessageServeur(ajax)
             {
-                console.log("recevoirMessageServeur");
-
                 let reponse = JSON.parse(ajax.responseText);
 
                 let message = reponse.message;
                 console.log("message = " + message);
                 if(message==="enregistré")
-                    pDescription.innerHTML = "Description :<br> " + (input.value+"").split("\n").join("<br>");
+                    pDescription.innerHTML = "Description :<br> " + (inputDescription.value+"").split("\n").join("<br>");
                 if(message==="supprimé")
                     divOffre.parentNode.removeChild(divOffre);
+            }
+
+            function initListenersWithID(id) {
+                pDescription = document.getElementById(id+"description");
+
+                let pDescriptionEvent = function () {
+                    let text = pDescription.innerHTML.substr(18,pDescription.innerHTML.length);
+                    let idInput = Math.random() + "";
+                    pDescription.innerHTML ="<textarea id='"+idInput+"' style='height: 100%;'>"+text.split("<br>").join("\n")+"</textarea>";
+                    inputDescription = document.getElementById(idInput);
+                    inputDescription.addEventListener("mouseout",function(){
+                        //Ajax
+                        let ajax = new Ajax();
+                        ajax.executer("POST", "../scripts/ajax-modifier-offre.php?id="+id+"&attribut=description&valeur="+inputDescription.value,"",recevoirMessageServeur);
+                        pDescription.addEventListener("click",pDescriptionEvent);
+                    });
+                    pDescription.removeEventListener("click",arguments.callee);
+                };
+
+                pDescription.addEventListener("click",pDescriptionEvent);
             }
         </script>
     </head>
@@ -40,25 +62,40 @@
         foreach (recupererDestinations() as $tab) {
         ?>
                 <div class="div-destination-admin" id="<?= $tab["id_offre"] ?>">
+                    <script>
+                        document.getElementById("<?= $tab["id_offre"]?>").addEventListener("mouseenter",function () {
+                            idOffre = <?= $tab["id_offre"]?>;
+                            initListenersWithID(idOffre);
+
+                            let aOffre = document.getElementById("supprimer_"+idOffre);
+                            aOffre.removeAttribute("href");
+                            divOffre = document.getElementById(idOffre);
+                            aOffre.addEventListener("click", function(){
+                                let ajax = new Ajax();
+                                console.log(ajax);
+                                ajax.executer("POST", "../scripts/ajax-suppression-offre.php?id="+idOffre,"",recevoirMessageServeur);
+                            });
+                        })
+                    </script>
                     <p id="message_serveur_modif"></p>
                     <div class="destination-admin-item">
                         <img src="<?= $tab["url_image"] ?>" class="img-destination" alt="Image représentant la destination"/>
                         <!--<button id="change_image">Change</button>-->
                     </div>
                     <div class="destination-admin-item">
-                        <p>Ville : <?= $tab["ville"] ?></p>
+                        <p id="<?= $tab["id_offre"] . "ville"?>">Ville : <?= $tab["ville"] ?></p>
                         <!--<button id="change_ville">Change</button>-->
                     </div>
                     <div class="destination-admin-item">
-                        <p>Prix : <?= $tab["prix"] ?>€</p>
+                        <p id="<?= $tab["id_offre"] . "prix"?>">Prix : <?= $tab["prix"] ?>€</p>
                         <!--<button id="change_prix">Change</button>-->
                     </div>
                     <div class="destination-admin-item">
-                        <p>Début : <?= $tab["debut"] ?></p>
+                        <p id="<?= $tab["id_offre"] . "debut"?>">Début : <?= $tab["debut"] ?></p>
                         <!--<button id="change_date">Change</button>-->
                     </div>
                     <div class="destination-admin-item">
-                        <p>Durée : <?= $tab["duree"] ?> jours</p>
+                        <p id="<?= $tab["id_offre"] . "duree"?>">Durée : <?= $tab["duree"] ?> jours</p>
                         <!--<button id="change_duree">Change</button>-->
                     </div>
                     <div class="destination-admin-item" onclick="">
@@ -116,34 +153,4 @@
         <hr/>
         <?php include("footer.html"); ?>
     </body>
-    <script>
-        let idOffre = 5;
-        let pDescription = document.getElementById(idOffre+"description");
-        let pDescriptionEvent = function () {
-            let text = pDescription.innerHTML.substr(18,pDescription.innerHTML.length);
-            let idInput = Math.random() + "";
-            pDescription.innerHTML ="<textarea id='"+idInput+"' style='height: 100%;'>"+text.split("<br>").join("\n")+"</textarea>";
-            inputDescription = document.getElementById(idInput);
-            inputDescription.addEventListener("mouseout",function(){
-                //Ajax
-                let ajax = new Ajax();
-                ajax.executer("POST", "../scripts/ajax-modifier-offre.php?id="+idOffre+"&attribut=description&valeur="+inputDescription.value,"",recevoirMessageServeur);
-                pDescription.addEventListener("click",pDescriptionEvent);
-            });
-            pDescription.removeEventListener("click",arguments.callee);
-        };
-
-        pDescription.addEventListener("click",pDescriptionEvent);
-    </script>
-    <script>
-        let id_offre = 1;
-        let aOffre = document.getElementById("supprimer_"+id_offre);
-        aOffre.removeAttribute("href");
-        let divOffre = document.getElementById(id_offre);
-        aOffre.addEventListener("click", function(){
-            let ajax = new Ajax();
-            console.log(ajax);
-            ajax.executer("POST", "../scripts/ajax-suppression-offre.php?id="+id_offre,"",recevoirMessageServeur);
-        });
-    </script>
 </html>
